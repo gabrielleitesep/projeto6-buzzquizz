35d6d31f71ec9682181d9d65f6687b0aec813046
Matheus Rodrigo Machado Melo <mathrmm@pop-os.localdomain>
2022-06-29 20:22:27 -0300

    tela3 meio

---
+++ src/tela3.js
@@ -0,0 +1,15 @@
+let quizz = [{
+    image: "https",
+    text: "irineu"
+},
+{
+    image: "https",
+    text: "irineu"
+}]
+
+
+function test(){
+localStorage.setItem("usuario",JSON.stringify(quizz))
+
+}
+test()
