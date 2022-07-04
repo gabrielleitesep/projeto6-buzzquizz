33fdd5d33c65551dee0418901298dce1b4c32d7a
Matheus Rodrigo Machado Melo <matheusrodrigomm@gmail.com>
2022-07-04 16:17:45 -0300

    Ajuste tela1

--- src/index.js
+++ src/index.js
@@ -59,42 +59,10 @@ function renderizarQuizzLocal() {
   }
 }
 
+
+
 function renderMain(){
-  let homepage = document.querySelector(body);
-  homepage.innerHTML = `
-  <div class="root">
-  <header>
-    <h1>BuzzQuizz</h1>
-  </header>
-  <main>
-    <div class="tela1">
-      <div class="quizzUsuario">
-        <div class="semQuizz">
-          <div onclick="">
-            <h1>Você não criou nenhum quizz ainda :(</h1>
-            <button onclick="renderTela3()">Criar quizz</button>
-          </div>
-        </div>
-        <div class="quizzSalvos">
-          <div>
-            <div>
-              <h1>Seus Quizzes</h1>
-              <ion-icon name="add-circle-sharp" onclick="renderTela3()"></ion-icon>
-            </div>
-            <ul>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="container">
-        <h1>Todos os Quizzes</h1>
-        <ul>
-        </ul>
-      </div>
-    </div>
-  </main>
-  </div>
-`
+  window.location.reload()
 }
 
 function returnMain(){
